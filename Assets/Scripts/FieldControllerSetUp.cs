c3355adbae658fd9f1e06d19c2d825f77f08f3f9
a01177702 <a01177702@tec.mx>
2023-09-05 21:52:27 -0600

    updateHarvesters

--- Assets/Scripts/FieldControllerSetUp.cs
+++ Assets/Scripts/FieldControllerSetUp.cs
@@ -4,21 +4,23 @@ using UnityEngine;
 public class FieldControllerSetUp : MonoBehaviour
 {
     public Transform fieldTransform;
-    public GameObject unitPrefab;  // Corn section prefab
+    public GameObject unitPrefab;  
+    public GameObject unitPrefab2; 
 
-    private WS_Client wsClient; 
+    private WS_Client wsClient;
 
     void Awake()
     {
         CreateGlobalMatrix(); // Create the matrix of corn sections on GlobalData
     }
+
     void Start()
     {
         CreateField();
         UpdateParentPosition();
-        // wsClient = FindObjectOfType<WS_Client>(); 
+        // wsClient = FindObjectOfType<WS_Client>();
 
-        GlobalData.harvesters = FindObjectsOfType<Harvester>(); 
+        GlobalData.harvesters = FindObjectsOfType<Harvester>();
     }
 
     void CreateGlobalMatrix()
@@ -27,9 +29,9 @@ public class FieldControllerSetUp : MonoBehaviour
         int[,] matrix = new int[GlobalData.fieldRows, GlobalData.fieldCols];
 
         // Fill the matrix with 1s (not harvested)
-        for(int i = 0; i < GlobalData.fieldRows; i++)
+        for (int i = 0; i < GlobalData.fieldRows; i++)
         {
-            for(int j = 0; j < GlobalData.fieldCols; j++)
+            for (int j = 0; j < GlobalData.fieldCols; j++)
             {
                 matrix[i, j] = 1;
             }
@@ -41,7 +43,7 @@ public class FieldControllerSetUp : MonoBehaviour
 
         // wsClient.SendCampo(matrix);
     }
-    
+
     void CreateField()
     {
         for (int row = 0; row < GlobalData.fieldRows; row++) // Start generating from top to bottom
@@ -49,7 +51,7 @@ public class FieldControllerSetUp : MonoBehaviour
             for (int col = 0; col < GlobalData.fieldCols; col++) // Generate from left to right
             {
                 GameObject newUnit = Instantiate(unitPrefab, transform);
-                
+
                 // Set the position of the new corn section
                 newUnit.transform.position = new Vector3(col * GlobalData.unit_xSize, 0, row * GlobalData.unit_zSize);
                 newUnit.name = $"Unit({row}, {col})";
@@ -59,8 +61,37 @@ public class FieldControllerSetUp : MonoBehaviour
 
     void UpdateParentPosition()
     {
-        transform.position += new Vector3(GlobalData.unit_xSize / 2, 0, GlobalData.unit_zSize / 2); 
+        transform.position += new Vector3(GlobalData.unit_xSize / 2, 0, GlobalData.unit_zSize / 2);
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Debug.Log("tecla");
+            AddColumn();
+            
+        }
+       
     }
 
-    
+
+void AddColumn()
+{
+    int newCol = GlobalData.fieldCols; 
+    GlobalData.fieldCols++; // 
+
+    // pasa por cada row y pone uno 
+    for (int row = 0; row < GlobalData.fieldRows; row++)
+    {
+        GameObject newUnit = Instantiate(unitPrefab2, transform);
+
+        // calcula posicion
+        Vector3 newPosition = new Vector3(newCol * GlobalData.unit_xSize, 0, row * GlobalData.unit_zSize);
+
+        // set y nonmbre me lo copie de arturo 
+        newUnit.transform.position = newPosition;
+        newUnit.name = $"Unit({row}, {newCol})";
+
+        Debug.Log($"Created Unit({row}, {newCol})");
+    }
+}
+
 }
--- Assets/Scripts/HarvesterPlacement.cs
+++ Assets/Scripts/HarvesterPlacement.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class HarvesterPlacement : MonoBehaviour
 {
     public GameObject harvesterPrefab; // The harvester prefab to instantiate
-    public int numHarvestersToPlace = 2; // Number of harvesters to place
     public float harvesterSpacing = 2.0f; // Spacing between harvesters
+
+    private List<GameObject> harvesters = new List<GameObject>(); // Keep track of the spawned harvesters
 
     private void Start()
     {
         PlaceHarvesters();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AddHarvester();
+        }
+    }
+
     void PlaceHarvesters()
     {
+        int numHarvestersToPlace = GlobalData.numTrucks; // Get the number of harvesters from GlobalData
+
         // Calculate the total width required for all harvesters
         float totalWidth = (numHarvestersToPlace - 1) * harvesterSpacing;
 
@@ -25,13 +36,27 @@ public class HarvesterPlacement : MonoBehaviour
         {
             GameObject newHarvester = Instantiate(harvesterPrefab);
 
-            // Calculate the x position with an offset
+            // x position with an offset
             float xPosition = startX + i * harvesterSpacing;
 
             // Adjust the z position if needed
             float zPosition = -GlobalData.unit_zSize * 0.99f;
 
             newHarvester.transform.position = new Vector3(xPosition, 0, zPosition);
+
+            harvesters.Add(newHarvester); // Add the harvester to the list
         }
+    }
+
+    void AddHarvester()
+    {
+        GameObject lastHarvester = harvesters[harvesters.Count - 1];
+        float xPosition = lastHarvester.transform.position.x + harvesterSpacing;
+
+        // z position
+        float zPosition = -GlobalData.unit_zSize * 0.99f;
+
+        GameObject newHarvester = Instantiate(harvesterPrefab, new Vector3(xPosition, 0, zPosition), Quaternion.identity);
+        harvesters.Add(newHarvester);
     }
 }
