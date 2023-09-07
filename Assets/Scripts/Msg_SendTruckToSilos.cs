507c72b561a85ed9efe691d606bc28256a0e48ab
ArturoDuCas <a00833516@tec.mx>
2023-09-07 00:03:26 -0600

    waaaaa

---
+++ Assets/Scripts/Msg_SendTruckToSilos.cs
@@ -0,0 +1,12 @@
+﻿using System; 
+
+
+[Serializable]
+public class Msg_SendTruckToSilos
+{
+    public string type;
+    public string finalPos;
+    public string  fieldMatrix;
+    public string truckInitialPos; 
+    public string truckId;
+}
